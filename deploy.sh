5454c730e046e44b2a1d6110bebf9411cc7cb885
handhand <sohnjunior@naver.com>
2021-02-28 21:03:03 +0900

    [docs] deploy

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git checkout source
+git add .
+git commit -m "[docs] deploy"
+git branch -D master
+git checkout -b master
+git filter-branch --subdirectory-filter _site -f HEAD
+git push --all
+git checkout source
